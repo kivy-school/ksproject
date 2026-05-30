7a88cff1bb76668b07594b9a89d48094633a4239
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-05-30 16:36:13 +0530

    Basic GUI setup

---
+++ src/ksproject_gui/View/EntrypointScreen/entrypoint_screen.py
@@ -0,0 +1,10 @@
+from kivy.utils import platform
+
+from View.base_screen import BaseScreenView
+
+
+
+class EntrypointScreenView(BaseScreenView):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(EntrypointScreenView, self).__init__(*args, **kwargs)
